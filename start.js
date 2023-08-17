7205c53e156a451fe77c3d259e94124c54a1ca34
Oleksander Hatsko <sasha95.ak@gmail.com>
2023-08-17 21:13:02 +0300

    homework

---
+++ start.js
@@ -0,0 +1,19 @@
+// const object = {
+//   a: 10,
+//   b: 12,
+// };
+// const copy = object;
+// copy.c = 122;
+// console.table(object.c);
+// let inner = parseInt(prompt("enter number", "100"));
+//////////////////////
+// let a = 40;
+// let t = 40;
+// if (a > t) {
+//   a = 0;
+// } else if (a < t) {
+//   t = 0;
+// } else {
+//   (a = 1), (t = 1);
+// }
+// console.log(a, t);
